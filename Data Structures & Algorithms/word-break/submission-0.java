7bb7a1916960eea37bec38830ba453dee2f4d4b2
Sravan Surya <141347235+Sravan2808@users.noreply.github.com>
2026-09-08 12:02:37 +0530

    Add: word-break - submission-0

---
+++ Data Structures & Algorithms/word-break/submission-0.java
@@ -0,0 +1,22 @@
+class Solution {
+    private Boolean[] t;
+    int n;
+    private boolean solve(String s, int idx, List<String> wordDict) {
+        if (idx == n)
+            return true;
+        if (t[idx] != null)
+            return t[idx];
+        for (int end = idx + 1; end <= n; end++) {
+            String split = s.substring(idx, end);
+
+            if (wordDict.contains(split) && solve(s, end, wordDict))
+                return t[idx] = true;
+        }
+        return t[idx] = false;
+    }
+    public boolean wordBreak(String s, List<String> wordDict) {
+        n = s.length();
+        t = new Boolean[n];
+        return solve(s, 0, wordDict);
+    }
+}
